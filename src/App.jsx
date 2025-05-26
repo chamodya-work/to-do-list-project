1220f74efc0acddda5ca613be2a341b391c2f863
chamodya <chamodyarajapaksha1@gmail.com>
2025-05-26 20:31:22 +0530

    define todo structure

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,11 @@ function App() {
   return (
     <div className={styles.App}>
       <header className={styles.Header}>
-        <img className={styles.Logo} />
+        <img className={styles.Logo} src='/to-do.png' />
         <h2 className={styles.Title}>To-Do App</h2>
       </header>
 
-      <div className={styles.AppContainer}></div>
+      <div className={styles.AppContainer}>Content</div>
     </div>
   );
 }
