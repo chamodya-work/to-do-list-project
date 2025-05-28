155b7f527a2059fcc24a2c7134b43ab7c6262d92
chamodya <chamodyarajapaksha1@gmail.com>
2025-05-28 11:18:36 +0530

    add stylig to TodoList

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,25 +1,40 @@
+import styles from "./TodoList.module.css";
+
 export function TodoList({ todos }) {
     return (
         <section>
             <h3>To-Do's</h3>
 
-            <ul>
+            <ul className={styles.TodoList}>
                 {todos.map((todo) => (
-                    <li key={todo.id}>
-                        <input type="checkbox" name="completed" defaultChecked={todo.completed} />
-                        <div>
-                            {todo.name}
-                            <br />
-                            {todo.description}
-                            <br />
-                            {todo.deadline} {todo.priority !== "none" && todo.priority}
+                    <li
+                        key={todo.id}
+                        className={styles.TodoListItem}
+                        data-completed={todo.completed}
+                    >
+                        <div className={styles.Content}>
+                            <input
+                                type="checkbox"
+                                name="completed"
+                                defaultChecked={todo.completed}
+                                className={styles.Status}
+                            />
 
+                            <div className={styles.Info}>
+                                {todo.name}
+
+                                {todo.description && (
+                                    <span className={styles.Description}>{todo.description}</span>
+                                )}
+
+                                <div className={styles.AdditionalInfo}>
+                                    {todo.deadline} {todo.priority !== "none" && todo.priority}
+                                </div>
+                            </div>
                         </div>
-
                     </li>
                 ))}
             </ul>
-
         </section>
     );
 }
