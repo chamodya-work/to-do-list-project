c8ef80d4448a51b84a5161dd27276e0e81034de7
chamodya <chamodyarajapaksha1@gmail.com>
2025-06-07 00:37:05 +0530

    add css module to filters

--- src/components/TodoFilters/TodoFilters.jsx
+++ src/components/TodoFilters/TodoFilters.jsx
@@ -1,20 +1,26 @@
+import styles from './TodoFilters.module.css';
 export function TodoFilters() {
     return (
-        <section>Add commentMore actions
+        <section>
             <h3>Filters</h3>
-
             <div className={styles.Filters}>
                 <label htmlFor="completed">Completed</label>
-                <select
-                    id="completed"
-                    defaultValue={completed}
-                    onChange={(event) => setCompleted(event.target.value)}
-                >
+                <select id="completed" defaultValue="all">
                     <option value="all">All</option>
-                    <option value="active">Action</option>
+                    <option value="active" >Active</option>
                     <option value="completed">Completed</option>
+                </select>
+                <label htmlFor="priority">Priority</label>
+                <select id="priority" defaultValue="all">
+                    <option value="all">All</option>
+                    <option value="low" >Low</option>
+                    <option value="medium">Medium</option>
+                    <option value="high">High</option>
 
+                </select>
             </div>
+
         </section>
     )
+
 }
