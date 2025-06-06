7fac8cc51554cd3b2630089714cf51b09470d7e0
chamodya <chamodyarajapaksha1@gmail.com>
2025-06-07 00:48:05 +0530

    add useSyaye to set filters

--- src/components/TodoFilters/TodoFilters.jsx
+++ src/components/TodoFilters/TodoFilters.jsx
@@ -1,17 +1,20 @@
+import { useState } from 'react';
 import styles from './TodoFilters.module.css';
 export function TodoFilters() {
+    const [completed, setComplted] = useState("all");
+    const [priority, setPriority] = useState("all");
     return (
         <section>
             <h3>Filters</h3>
             <div className={styles.Filters}>
                 <label htmlFor="completed">Completed</label>
-                <select id="completed" defaultValue="all">
+                <select id="completed" defaultValue={completed} onChange={(event) => setComplted(event.target.value)}>
                     <option value="all">All</option>
                     <option value="active" >Active</option>
                     <option value="completed">Completed</option>
                 </select>
                 <label htmlFor="priority">Priority</label>
-                <select id="priority" defaultValue="all">
+                <select id="priority" defaultValue={priority} onChange={(event) => setPriority(event.target.value)}>
                     <option value="all">All</option>
                     <option value="low" >Low</option>
                     <option value="medium">Medium</option>
