c1b986fa077bd2f68f292f0d08f1ad59447b6e8a
chamodya <chamodyarajapaksha1@gmail.com>
2025-06-06 23:59:49 +0530

    add filter1

--- src/components/TodoFilters/TodoFilters.jsx
+++ src/components/TodoFilters/TodoFilters.jsx
@@ -1,3 +1,20 @@
 export function TodoFilters() {
-    return (<div>filter</div>)
+    return (
+        <section>Add commentMore actions
+            <h3>Filters</h3>
+
+            <div className={styles.Filters}>
+                <label htmlFor="completed">Completed</label>
+                <select
+                    id="completed"
+                    defaultValue={completed}
+                    onChange={(event) => setCompleted(event.target.value)}
+                >
+                    <option value="all">All</option>
+                    <option value="active">Action</option>
+                    <option value="completed">Completed</option>
+
+            </div>
+        </section>
+    )
 }
