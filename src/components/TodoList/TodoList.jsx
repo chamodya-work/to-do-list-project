9858d8f3bc7c630743b1647f48a800ae8a9a5edc
chamodya <chamodyarajapaksha1@gmail.com>
2025-06-06 23:15:21 +0530

    add warning msg to show todo are ended

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -6,6 +6,7 @@ export function TodoList({ todos, onUpdate, onDelete }) {
     return (
         <section>
             <h3>To-Do's</h3>
+            {!todos.length && <p>⚠️Sorry, you don't have To-Do's</p>}
 
             <ul className={styles.TodoList}>
                 {todos.map((todo) => (
