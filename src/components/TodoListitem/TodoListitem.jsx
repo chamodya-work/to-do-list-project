39315549863e387740860b165022480d2fba0231
chamodya <chamodyarajapaksha1@gmail.com>
2025-06-22 23:54:51 +0530

    add validation to editing tab

--- src/components/TodoListitem/TodoListitem.jsx
+++ src/components/TodoListitem/TodoListitem.jsx
@@ -4,78 +4,72 @@ import { PRIORITIES, PRIORITY_DEFAULT } from "../../constants/priorities";
 import styles from "./TodoListitem.module.css";
 import { TodoFormFields } from "../TodoFormFields/TodoFormFields";
 export function TodoListItem({ todo, onUpdate, onDelete }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ defaultValues: todo });
 
+  function handleCompleted(event) {
+    onUpdate(todo.id, { ...todo, completed: event.target.checked });
+  }
 
-    const [isEditing, setIsEditing] = useState(false)
-    const { register, handleSubmit } = useForm({ defaultValues: todo });
+  function handleEdit(data) {
+    onUpdate(todo.id, data);
+    setIsEditing(false);
+  }
 
+  const viewingTemplate = (
+    <div className={styles.Content}>
+      <input
+        type="checkbox"
+        name="completed"
+        checked={todo.completed}
+        onChange={handleCompleted}
+        className={styles.Status}
+      />
 
-    function handleCompleted(event) {
-        onUpdate(todo.id, { ...todo, completed: event.target.checked });
-    }
+      <div className={styles.Info}>
+        {todo.name}
 
+        {todo.description && (
+          <span className={styles.Description}>{todo.description}</span>
+        )}
 
-    function handleEdit(data) {
-        onUpdate(todo.id, data)
-        setIsEditing(false);
-    }
-
-    const viewingTemplate = (
-        <div className={styles.Content}>
-            <input
-                type="checkbox"
-                name="completed"
-                checked={todo.completed}
-                onChange={handleCompleted}
-                className={styles.Status}
-            />
-
-            <div className={styles.Info}>
-                {todo.name}
-
-                {todo.description && (
-                    <span className={styles.Description}>{todo.description}</span>
-                )}
-
-                <div className={styles.AdditionalInfo}>
-                    {todo.deadline}{" "}
-                    {todo.priority !== PRIORITY_DEFAULT && PRIORITIES[todo.priority] && (
-                        <span style={{ color: PRIORITIES[todo.priority].color }}>
-                            {PRIORITIES[todo.priority].label}
-                        </span>
-                    )}
-
-                </div>
-
-            </div>
-            <div className={styles.Controls}>
-                <button onClick={() => setIsEditing(true)}>📝</button>
-                <button onClick={() => onDelete(todo.id)}>🗑️</button>
-
-            </div>
+        <div className={styles.AdditionalInfo}>
+          {todo.deadline}{" "}
+          {todo.priority !== PRIORITY_DEFAULT && PRIORITIES[todo.priority] && (
+            <span style={{ color: PRIORITIES[todo.priority].color }}>
+              {PRIORITIES[todo.priority].label}
+            </span>
+          )}
         </div>
-    )
+      </div>
+      <div className={styles.Controls}>
+        <button onClick={() => setIsEditing(true)}>📝</button>
+        <button onClick={() => onDelete(todo.id)}>🗑️</button>
+      </div>
+    </div>
+  );
 
-    const editingTemplate = (
-        <form className={styles.Content} onReset={() => setIsEditing(false)} onSubmit={handleSubmit(handleEdit)}>
-            <TodoFormFields todo={todo} register={register} />
-            <div className="styles.Controls}>">
-                <input type="submit" value="💾" />
-                <input type="reset" value="❌" />
-            </div>
+  const editingTemplate = (
+    <form
+      className={styles.Content}
+      onReset={() => setIsEditing(false)}
+      onSubmit={handleSubmit(handleEdit)}
+    >
+      <TodoFormFields todo={todo} register={register} errors={errors} />
+      <div className="styles.Controls}>">
+        <input type="submit" value="💾" />
+        <input type="reset" value="❌" />
+      </div>
+    </form>
+  );
 
-
-        </form >
-    )
-
-    return (
-        <li
-            className={styles.TodoListItem}
-            data-completed={todo.completed}
-        >
-            {isEditing ? editingTemplate : viewingTemplate}
-
-        </li>
-
-    );
+  return (
+    <li className={styles.TodoListItem} data-completed={todo.completed}>
+      {isEditing ? editingTemplate : viewingTemplate}
+    </li>
+  );
 }
