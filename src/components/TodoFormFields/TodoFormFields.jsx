5d92adade936ccd562920be99a0489821b64d207
chamodya <chamodyarajapaksha1@gmail.com>
2025-06-22 23:27:30 +0530

    fix the issue in the priority secton

--- src/components/TodoFormFields/TodoFormFields.jsx
+++ src/components/TodoFormFields/TodoFormFields.jsx
@@ -1,78 +1,93 @@
 import { PRIORITIES, PRIORITY_DEFAULT } from "../../constants/priorities";
 import styles from "./TodoFormFields.module.css";
 
-export function TodoFormFields({ todo = {}, showAllFields = true, register, errors = {} }) {
-    return (
-        <div className={styles.FormFields}>
-            <div className={styles.FormField}>
-                <input
-                    type="text"
-                    aria-label="Name*"
-                    placeholder="Name*"
-                    autoComplete="off"
-                    defaultValue={todo.name}
+export function TodoFormFields({
+  todo = {},
+  showAllFields = true,
+  register,
+  errors = {},
+}) {
+  return (
+    <div className={styles.FormFields}>
+      <div className={styles.FormField}>
+        <input
+          type="text"
+          aria-label="Name*"
+          placeholder="Name*"
+          autoComplete="off"
+          defaultValue={todo.name}
+          {...register("name", {
+            required: "name is required",
+            minLength: {
+              value: 3,
+              message: "Name must be at least 3 characters long",
+            },
+            maxLength: {
+              value: 50,
+              message: "Name must be at most 50 characters long",
+            },
+          })}
+        />
+        {!!errors.name && errors.name.message}
+      </div>
 
-                    {...register("name", {
-                        required: "name is required",
-                        minLength: {
-                            value: 3,
-                            message: "Name must be at least 3 characters long"
-                        },
-                        maxLength: {
-                            value: 50,
-                            message: "Name must be at most 50 characters long"
-                        }
-                    })}
-                />
+      {showAllFields && (
+        <>
+          <div className={styles.FormField}>
+            <textarea
+              aria-label="Description"
+              placeholder="Description"
+              rows="3"
+              defaultValue={todo.description}
+              {...register("description", {
+                maxLength: {
+                  value: 50,
+                  message: "Description must be at most 50 characters long",
+                },
+              })}
+            />
+            {!!errors.description && errors.description.message}
+          </div>
+
+          <div className={styles.FormGroup}>
+            <div className={styles.FormField}>
+              <label htmlFor="deadline">Deadline</label>
+              <input
+                type="date"
+                id="deadline"
+                defaultValue={todo.deadline}
+                {...register("deadline", {
+                  min: !todo.id && {
+                    value: new Date().toISOString().split("T")[0],
+                    message: "Deadline cant be date in the past",
+                  },
+                })}
+              />
+              {!!errors.deadline && errors.deadline.message}
             </div>
 
-            {showAllFields && (
-                <><div className={styles.FormField}>
-                    <textarea
-                        aria-label="Description"
-                        placeholder="Description"
-                        rows="3"
-                        defaultValue={todo.description}
-                        {...register("description", {
-                            maxLength: {
-                                value: 200,
-                                message: "Description must be at most 200 characters long"
-                            }
-                        })}
-                    />
-
-                </div>
-
-                    <div className={styles.FormGroup}>
-                        <div className={styles.FormField}>
-                            <label htmlFor="deadline">Deadline</label>
-                            <input type="date" id="deadline" defaultValue={todo.deadline}
-                                {...register("deadline", {
-                                    min: !todo.id && {
-                                        value: new Date().toISOString().split("T")[0],
-                                        message: "Deadline cant be date in the past"
-                                    }
-                                }
-                                )
-                                }
-                            />
-                        </div>
-
-                        <div className={styles.FormField}>
-                            <label htmlFor="priority">Priority</label>
-                            <select defaultValue={todo.priority ?? PRIORITY_DEFAULT} id="priority"
-                                {...register("priority")}
-                            >
-
-                                {Object.entries(PRIORITIES).map(([key, { label }]) =>
-
-                                    <option key={key} value="key">{label}</option>
-                                )}
-                            </select>
-                        </div>
-                    </div>
-                </>)
-            }
-        </div>
-    )
+            <div className={styles.FormField}>
+              <label htmlFor="priority">Priority</label>
+              <select
+                defaultValue={todo.priority ?? PRIORITY_DEFAULT}
+                id="priority"
+                {...register("priority", {
+                  validate: (value) =>
+                    Object.keys(PRIORITIES).includes(value) ||
+                    "prority is not valid",
+                })}
+              >
+                {Object.entries(PRIORITIES).map(([key, { label }]) => (
+                  <option key={key} value={key}>
+                    {label}
+                  </option>
+                ))}
+              </select>
+              {!!errors.priority && errors.priority.message}
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
