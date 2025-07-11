d95a72ddd8281626d3ca6a93ed94a9939b2b5364
chamodya <chamodyarajapaksha1@gmail.com>
2025-07-11 22:28:07 +0530

    modified TodoForm and TodoListItem with schema and yup resolver

--- src/components/TodoForm/TodoForm.jsx
+++ src/components/TodoForm/TodoForm.jsx
@@ -1,44 +1,50 @@
-
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { PRIORITY_DEFAULT } from "../../constants/priorities";
 import styles from "./TodoForm.module.css";
 import { TodoFormFields } from "../TodoFormFields/TodoFormFields";
+import { getTodoSchema } from "../../schemas/todo";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 export function TodoForm({ onCreate }) {
+  const [showAllFields, setShowAllFields] = useState(true);
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(getTodoSchema()),
+    defaultValues: {
+      description: "",
+      deadline: "",
+      priority: PRIORITY_DEFAULT,
+      completed: false,
+    },
+  });
 
-    const [showAllFields, setShowAllFields] = useState(true)
-    const { register, handleSubmit, reset, formState: { errors } } = useForm(
-        {
-            defaultValues: {
-                description: "",
-                deadline: "",
-                priority: PRIORITY_DEFAULT,
-                completed: false
+  function handleCreate(data) {
+    onCreate(data);
+    reset();
+  }
+  return (
+    <section>
+      <h3 className={styles.Title}>
+        New To-Do
+        <button onClick={() => setShowAllFields(!showAllFields)}>
+          {showAllFields ? "Hide" : "show"} all fields
+        </button>
+      </h3>
 
-            }
-        }
-    )
+      <form className={styles.Form} onSubmit={handleSubmit(handleCreate)}>
+        <TodoFormFields
+          showAllFields={showAllFields}
+          register={register}
+          errors={errors}
+        />
 
-
-    function handleCreate(data) {
-        onCreate(data);
-        reset();
-
-    }
-    return (
-        <section>
-            <h3 className={styles.Title}>New To-Do
-                <button onClick={() => setShowAllFields(!showAllFields)}>
-                    {showAllFields ? "Hide" : "show"} all fields</button>
-            </h3>
-
-            <form className={styles.Form} onSubmit={handleSubmit(handleCreate)}>
-
-                <TodoFormFields showAllFields={showAllFields} register={register} errors={errors} />
-
-                <input type="submit" value="Add" />
-            </form>
-        </section>
-    );
+        <input type="submit" value="Add" />
+      </form>
+    </section>
+  );
 }
--- src/components/TodoListitem/TodoListitem.jsx
+++ src/components/TodoListitem/TodoListitem.jsx
@@ -3,13 +3,18 @@ import { useForm } from "react-hook-form";
 import { PRIORITIES, PRIORITY_DEFAULT } from "../../constants/priorities";
 import styles from "./TodoListitem.module.css";
 import { TodoFormFields } from "../TodoFormFields/TodoFormFields";
+import { getTodoSchema } from "../../schemas/todo";
+import { yupResolver } from "@hookform/resolvers/yup";
+
 export function TodoListItem({ todo, onUpdate, onDelete }) {
   const [isEditing, setIsEditing] = useState(false);
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm({ defaultValues: todo });
+  } = useForm(
+    resolver: yupResolver(getTodoSchema()),
+    { defaultValues: todo });
 
   function handleCompleted(event) {
     onUpdate(todo.id, { ...todo, completed: event.target.checked });
