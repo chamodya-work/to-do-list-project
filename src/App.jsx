27385f247876ea59c05013c2dddb0ed2e2de69f6
chamodya <chamodyarajapaksha1@gmail.com>
2025-07-17 01:44:20 +0530

    move todos to api

--- src/App.jsx
+++ src/App.jsx
@@ -1,85 +1,55 @@
-import { useState } from 'react';
-import { TodoForm } from './components/TodoForm/TodoForm';
-import { TodoList } from './components/TodoList/TodoList';
-import styles from './App.module.css'
-import { TodoFilters } from './components/TodoFilters/TodoFilters';
-
-const TODOS_DEFAULT = [
-  {
-    id: "1",
-    name: "Buy an Ice Cream",
-    description: "The white one with chocolate",
-    deadline: "2025-02-09",
-    priority: "low",
-    completed: false,
-  },
-  {
-    id: "2",
-    name: "Sell old MacBook Pro 2025",
-    description: "Try to sell it on OLX",
-    deadline: "2025-02-28",
-    priority: "high",
-    completed: false,
-  },
-  {
-    id: "3",
-    name: "Charge Powerbank",
-    description: "For the next travelling",
-    deadline: "2025-02-15",
-    priority: "medium",
-    completed: true,
-  },
-  {
-    id: "4",
-    name: "Test Todo only with a name",
-    description: "",
-    deadline: "",
-    priority: "none",
-    completed: false,
-  },
-];
-
+import { useState } from "react";
+import { TodoForm } from "./components/TodoForm/TodoForm";
+import { TodoList } from "./components/TodoList/TodoList";
+import styles from "./App.module.css";
+import { TodoFilters } from "./components/TodoFilters/TodoFilters";
 
 function App() {
-
-  const [todos, setTodos] = useState(TODOS_DEFAULT);
-  const [filters, setFilters] = useState({})
+  const [todos, setTodos] = useState([]);
+  const [filters, setFilters] = useState({});
 
   function handleCreate(newTodo) {
-    setTodos((prevTOdos) => [...prevTOdos, { id: `${prevTOdos.length + 1}`, ...newTodo }])
+    setTodos((prevTOdos) => [
+      ...prevTOdos,
+      { id: `${prevTOdos.length + 1}`, ...newTodo },
+    ]);
   }
 
   function handleUpdate(id, newTodo) {
-    setTodos((prevTOdos) => prevTOdos.map((todo) => todo.id === id ? newTodo : todo))
+    setTodos((prevTOdos) =>
+      prevTOdos.map((todo) => (todo.id === id ? newTodo : todo))
+    );
   }
   function handleDelete(id) {
-    setTodos((prevTOdos) => prevTOdos.filter((todo) => todo.id !== id))
+    setTodos((prevTOdos) => prevTOdos.filter((todo) => todo.id !== id));
   }
   function filterTodos(todo) {
-    const { completed, priority } = filters
+    const { completed, priority } = filters;
     return (
       (completed == "" || todo.completed == completed) &&
       (priority == "" || todo.priority == priority)
-    )
+    );
   }
 
   return (
     <div className={styles.App}>
       <header className={styles.Header}>
-        <img className={styles.Logo} src='/to-do.png' />
+        <img className={styles.Logo} src="/to-do.png" />
         <h2 className={styles.Title}>To-Do App</h2>
       </header>
-
 
       <div className={styles.AppContainer}>
         <TodoForm onCreate={handleCreate} />
         <TodoFilters onFilter={setFilters} />
 
-        <TodoList todos={todos.filter(filterTodos)} onUpdate={handleUpdate} onDelete={handleDelete} />
-
+        <TodoList
+          todos={todos.filter(filterTodos)}
+          onUpdate={handleUpdate}
+          onDelete={handleDelete}
+        />
       </div>
     </div>
   );
 }
 
-export default App
+export default App;
