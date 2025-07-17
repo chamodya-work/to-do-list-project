157edfb4c61382ef2493cec94b2af3f08e013230
chamodya <chamodyarajapaksha1@gmail.com>
2025-07-18 01:43:02 +0530

    fx the issue of 404 status

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -3,16 +3,21 @@ import { PRIORITIES, PRIORITY_DEFAULT } from "../../constants/priorities";
 import { TodoListItem } from "../TodoListitem/TodoListitem";
 
 export function TodoList({ todos, onUpdate, onDelete }) {
-    return (
-        <section>
-            <h3>To-Do's</h3>
-            {!todos.length && <p>⚠️Sorry, you don't have To-Do's</p>}
+  return (
+    <section>
+      <h3>To-Do's</h3>
+      {!todos.length && <p>⚠️Sorry, you don't have To-Do's</p>}
 
-            <ul className={styles.TodoList}>
-                {todos.map((todo) => (
-                    <TodoListItem key={todo.id} todo={todo} onUpdate={onUpdate} onDelete={onDelete} />
-                ))}
-            </ul>
-        </section >
-    );
+      <ul className={styles.TodoList}>
+        {todos.map((todo) => (
+          <TodoListItem
+            key={todo.id}
+            todo={todo}
+            onUpdate={onUpdate}
+            onDelete={onDelete}
+          />
+        ))}
+      </ul>
+    </section>
+  );
 }
