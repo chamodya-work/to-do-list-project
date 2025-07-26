f35dabeb0d1e620a441355ef362f31419f144069
chamodya <chamodyarajapaksha1@gmail.com>
2025-07-27 00:57:09 +0530

    move api calls and hooks to custom todo.js hook

--- src/App.jsx
+++ src/App.jsx
@@ -6,49 +6,6 @@ import { TodoFilters } from "./components/TodoFilters/TodoFilters";
 import { api } from "./api";
 
 function App() {
-  const [todos, setTodos] = useState([]);
-  const [filters, setFilters] = useState({});
-
-  async function fetchTodos() {
-    try {
-      const data = await api.todos.getAll(filters);
-      setTodos(data);
-    } catch (error) {
-      console.error("Failed to fetch todos:", error);
-    }
-  }
-
-  useEffect(() => {
-    fetchTodos();
-  }, [filters]);
-
-  async function handleCreate(newTodo) {
-    try {
-      await api.todos.create(newTodo);
-      await fetchTodos();
-    } catch (error) {
-      console.error("Failed to create todos:", error);
-    }
-  }
-
-  async function handleUpdate(id, newTodo) {
-    try {
-      await api.todos.update(id, newTodo);
-      await fetchTodos();
-    } catch (error) {
-      console.error("Failed to update todos:", error);
-    }
-  }
-
-  async function handleDelete(id) {
-    try {
-      await api.todos.delete(id);
-      await fetchTodos();
-    } catch (error) {
-      console.error("Failed to delete todos:", error);
-    }
-  }
-
   return (
     <div className={styles.App}>
       <header className={styles.Header}>
---
+++ src/hooks/todo.js
@@ -0,0 +1,58 @@
+import { useState, useEffect } from 'react';
+import { api } from '../api';
+
+export function useTodos(){
+    const [todos, setTodos] = useState([]);
+  const [filters, setFilters] = useState({});
+
+  async function fetchTodos() {
+    try {
+      const data = await api.todos.getAll(filters);
+      setTodos(data);
+    } catch (error) {
+      console.error("Failed to fetch todos:", error);
+    }
+  }
+
+  useEffect(() => {
+    fetchTodos();
+  }, [filters]);
+
+  async function handleCreate(newTodo) {
+    try {
+      await api.todos.create(newTodo);
+      await fetchTodos();
+    } catch (error) {
+      console.error("Failed to create todos:", error);
+    }
+  }
+
+  async function handleUpdate(id, newTodo) {
+    try {
+      await api.todos.update(id, newTodo);
+      await fetchTodos();
+    } catch (error) {
+      console.error("Failed to update todos:", error);
+    }
+  }
+
+  async function handleDelete(id) {
+    try {
+      await api.todos.delete(id);
+      await fetchTodos();
+    } catch (error) {
+      console.error("Failed to delete todos:", error);
+    }
+  }
+
+  return{
+    data: todos,
+    fetch: fetchTodos,
+    filter:setFilters,
+    create: handleCreate,
+    update: handleUpdate,
+    delete: handleDelete,
+  }
+
+}
+
