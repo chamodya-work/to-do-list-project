98c668eeaac4046687d65f9eeebc07582183b22e
chamodya <chamodyarajapaksha1@gmail.com>
2025-08-01 22:53:54 +0530

    apply loader animation for improve UI/UX

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { TodoForm } from "./components/TodoForm/TodoForm";
 import { TodoList } from "./components/TodoList/TodoList";
 import { Alert } from "./components/Alert/Alert";
+import { Loader } from "./components/Loader/Loader";
 import styles from "./App.module.css";
 import { TodoFilters } from "./components/TodoFilters/TodoFilters";
 import { useTodos } from "./hooks/todo";
@@ -11,13 +12,13 @@ function App() {
   const todos = useTodos();
   return (
     <div className={styles.App}>
-      <h2>{JSON.stringify(todos.isLoading)}</h2>
       <header className={styles.Header}>
         <img className={styles.Logo} src="/to-do.png" />
         <h2 className={styles.Title}>To-Do App</h2>
       </header>
 
       <div className={styles.AppContainer}>
+        {todos.isLoading && <Loader />}
         {!!todos.error.message && (
           <Alert onClear={todos.error.clear}> {todos.error.message}</Alert>
         )}
